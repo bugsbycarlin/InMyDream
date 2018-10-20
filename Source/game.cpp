bfcf5b468a64f4521840e8332e60ea57cd31e759
Matthew Carlin <bugsby.carlin@gmail.com>
2018-10-19 23:08:03 -0700

    Basic test of premise, demo for Jin. Yay!

--- Source/game.cpp
+++ Source/game.cpp
@@ -21,32 +21,64 @@ void Game::initialize() {
   screen_width = hot_config.getInt("layout", "screen_width");
   screen_height = hot_config.getInt("layout", "screen_height");
 
-  graphics.addImage("character_right", "Art/character_right.png");
-  graphics.addImage("character_left", "Art/character_left.png");
+  graphics.addImages("Art/", {
+    "character_left",
+    "character_right",
+    "basic_wall_corner_bottom_left",
+    "basic_wall_corner_bottom_right",
+    "basic_wall_corner_top_left",
+    "basic_wall_corner_top_right",
+    "basic_wall_horizontal",
+    "basic_wall_vertical",
+  });
 
   // Set a bunch of variables from configuration
   screen_color = hot_config.getString("layout", "screen_color");
+  camera_y = hot_config.getInt("game", "starting_y");
   x_position = hot_config.getInt("game", "starting_x");
   y_position = hot_config.getInt("game", "starting_y");
-  move_speed = hot_config.getFloat("game", "move_speed");
+  move_acceleration = hot_config.getFloat("game", "move_acceleration");
   move_decay = hot_config.getFloat("game", "move_decay");
+  gravity = hot_config.getFloat("game", "gravity");
+  terminal_velocity = hot_config.getFloat("game", "terminal_velocity");
+
+  x_velocity = 0;
+  y_velocity = 0;
 
   input.addActionKey("left", "left");
   input.addActionKey("right", "right");
+
+  credit_text = new Textbox(
+    hot_config.getString("layout", "credits_font_name"),
+    hot_config.getInt("layout", "credits_font_size"),
+    "a", origin, "#000000");
+
+  font_base_x = hot_config.getInt("layout", "credits_font_base_x");
+  font_base_y = hot_config.getInt("layout", "credits_font_base_y");
 }
 
 void Game::logic() {
   if (input.actionDown("left")) {
-    x_velocity -= move_speed;
+    x_velocity -= move_acceleration;
   }
 
   if (input.actionDown("right")) {
-    x_velocity += move_speed;
+    x_velocity += move_acceleration;
   }
 
   x_position += x_velocity;
 
   x_velocity *= move_decay;
+
+  y_velocity += gravity;
+  if (y_velocity > terminal_velocity) {
+    y_velocity = terminal_velocity;
+  }
+  y_position += y_velocity;
+
+  if (y_position > (screens - 1) * 900) {
+    y_position -= (screens - 2) * 900;
+  }
 
   // To do: quit when the quit button is pressed. Sheesh.
   if (input.threeQuickKey("escape")) {
@@ -61,13 +93,38 @@ void Game::render() {
   // Switch to 2D drawing mode
   graphics.draw2D();
 
+  int camera_offset = camera_y - y_position;
+
+  // Draw walls
+  for (int i = 0; i < layout.size(); i++) {
+    for (int j = 0; j < layout[i].size(); j++) {
+      if (layout[i][j] > 0) {
+        graphics.drawImage(wall[layout[i][j]], 180 * j, 180 * i + camera_offset);
+      }
+    }
+  }
+
+  // Draw credits
+  for (int i = 0; i < credits.size(); i++) {
+    for (int j = 0; j < credits[i].size(); j++) {
+      credit_text->setPosition({
+        font_base_x,
+        900 * (2 * i + 2) + 120 * j + font_base_y + camera_offset,
+      });
+      credit_text->setText(credits[i][j]);
+      credit_text->draw();
+    }
+  }
+
+  // Draw character
+  graphics.setColor("#FFFFFF", 1.0);
   if (x_velocity >= 0) {
-    graphics.drawImage("character_right", x_position, y_position);
+    graphics.drawImage("character_right", x_position, camera_y);
   } else {
-    graphics.drawImage("character_left", x_position, y_position);
+    graphics.drawImage("character_left", x_position, camera_y);
   }
 }
 
 Game::~Game() {
-
+  delete credit_text;
 }
--- Source/game.h
+++ Source/game.h
@@ -8,6 +8,7 @@
 
 #include <string>
 #include <array>
+#include <vector>
 
 #include "honey.h"
 
@@ -28,6 +29,7 @@ class Game : public Screen {
   ~Game();
 
   float x_velocity;
+  float y_velocity;
   int x_position;
   int y_position;
 
@@ -36,6 +38,109 @@ class Game : public Screen {
 
   std::string screen_color;
 
-  float move_speed;
+  int camera_y;
+
+  float move_acceleration;
   float move_decay;
+  float gravity;
+  float terminal_velocity;
+
+  int font_base_x;
+  int font_base_y;
+
+  Textbox* credit_text;
+
+  const int screens = 12;
+
+  const vector<vector<string>> credits = {
+    {"In My Dream I Was Falling",},
+    {"Original Dream", "Ben Bascom"},
+    {"Programming", "and Design", "Matthew Carlin"},
+    {"Art and Design", "Jin Cui"},
+    {"Sound", "Trace Ronning"},
+  };
+
+  const vector<string> wall = {
+    "nothing",
+    "basic_wall_vertical",
+    "basic_wall_horizontal",
+    "basic_wall_corner_bottom_left",
+    "basic_wall_corner_bottom_right",
+    "basic_wall_corner_top_left",
+    "basic_wall_corner_top_right",
+  };
+
+  const vector<vector<int>> layout = {
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 4, 2, 3, 0, 1},
+    {1, 0, 0, 6, 2, 5, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 4, 3, 0, 0, 0, 0, 1},
+    {1, 6, 5, 0, 0, 0, 0, 1},
+
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+
+    {1, 0, 0, 0, 4, 3, 0, 1},
+    {1, 0, 0, 0, 1, 1, 0, 1},
+    {1, 0, 4, 2, 5, 1, 0, 1},
+    {1, 0, 1, 0, 0, 1, 0, 1},
+    {1, 0, 6, 2, 2, 5, 0, 1},
+
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1},
+  };
 };
